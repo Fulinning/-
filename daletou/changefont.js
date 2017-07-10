7e5cc27fd193d89b19e20ac9ffe1c9ee21f10b96
Fulinning <funinc@163.com>
2017-07-10 22:39:50 +0800

    change file

--- daletou/changefont.js
+++
@@ -1,8 +0,0 @@
-!function(){
-  var fontsize,width
-  $(window).on('resize',function(){
-    width = $(window).width()
-    fontsize = (width/360)*16
-    $('html').css('font-size',fontsize)
-  })
-}()
